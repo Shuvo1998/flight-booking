b6c5592ff9ae801b4cb5368a144ec219e7bc07d4
shuvo1998 <sk.ray1998t@gmail.com>
2021-01-30 18:29:16 +0600

    getIncreased() added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function getIncreased() {
+    const ticketInput = document.getElementById('ticket-quantity');
+    const ticketCount = parseInt(ticketInput.value);
+    const newCount = ticketCount + 1;
+    ticketInput.value = newCount;
+    setSubtotal();
+    calculateVat();
+    calculateTotal();
+}
