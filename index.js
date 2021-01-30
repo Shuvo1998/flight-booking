f588c4b847ac8b30cddc77dbade5851aae68ac64
shuvo1998 <sk.ray1998t@gmail.com>
2021-01-30 18:39:00 +0600

    getDecreased() added

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ function getIncreased() {
     calculateVat();
     calculateTotal();
 }
+function getDecreased() {
+    const ticketInput = document.getElementById('ticket-quantity');
+    const ticketCount = parseInt(ticketInput.value);
+    const newCount = ticketCount - 1;
+    if (newCount == 0) {
+        ticketInput.value = 0;
+    } else {
+        ticketInput.value = newCount;
+    }
+    setSubtotal();
+    calculateVat();
+    calculateTotal();
+}
+
 function setSubtotal() {
     const t = parseInt(document.getElementById('ticket-quantity').value) * 150 + parseInt(document.getElementById('value-hundred').value) * 100;
     (document.getElementById('sub-total')).innerText = t;
