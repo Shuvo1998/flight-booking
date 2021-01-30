634b7d2a978ade9125d22631b064d30b693abee9
shuvo1998 <sk.ray1998t@gmail.com>
2021-01-30 19:43:22 +0600

    merged increaseTicketQuatity

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-function getIncreased() {
-    const ticketInput = document.getElementById('ticket-quantity');
-    const ticketCount = parseInt(ticketInput.value);
-    const newCount = ticketCount + 1;
-    ticketInput.value = newCount;
+
+function increaseTicketQuantity(id) {
+    const ticketQuantity = document.getElementById(id);
+    const ticketCountNo = parseInt(ticketQuantity.value);
+    const newCount = ticketCountNo + 1;
+    ticketQuantity.value = newCount;
     setSubtotal();
     calculateVat();
     calculateTotal();
+
 }
 function getDecreased() {
     const ticketInput = document.getElementById('ticket-quantity');
     const ticketCount = parseInt(ticketInput.value);
     const newCount = ticketCount - 1;
-    ticketInput.value = newCount <= 0 ? 0 :newCount;
+    ticketInput.value = newCount <= 0 ? 0 : newCount;
     setSubtotal();
     calculateVat();
     calculateTotal();
 }
-function getIncreasedSecond() {
-    const ticketInputSecond = document.getElementById('value-hundred');
-    const ticketCountSecond = parseInt(ticketInputSecond.value);
-    const newCountSecond = ticketCountSecond + 1;
-    ticketInputSecond.value = newCountSecond;
-    setSubtotal();
-    calculateVat();
-    calculateTotal();
-}
+
 function getDecreasedSecond() {
     const ticketInputSecond = document.getElementById('value-hundred');
     const ticketCountSecond = parseInt(ticketInputSecond.value);
@@ -38,28 +32,28 @@ function setSubtotal() {
     const t = parseInt(document.getElementById('ticket-quantity').value) * 150 + parseInt(document.getElementById('value-hundred').value) * 100;
     (document.getElementById('sub-total')).innerText = t;
 }
-function getSubtotal(){
+function getSubtotal() {
     const t = parseInt(document.getElementById('ticket-quantity').value) * 150 + parseInt(document.getElementById('value-hundred').value) * 100;
     return (document.getElementById('sub-total')).innerText = t;
 }
-function calculateVat(){
+function calculateVat() {
     const Cost = document.getElementById('sub-total')
     const totalCost = parseInt(Cost.innerText);
     document.getElementById('total-vat').innerText = totalCost * 0.1;
 }
-function getVat(){
+function getVat() {
     const Cost = document.getElementById('sub-total')
     const totalCost = parseInt(Cost.innerText);
     return document.getElementById('total-vat').innerText = totalCost * 0.1;
 }
-function calculateTotal(){
+function calculateTotal() {
     document.getElementById('total').innerText = getSubtotal() + getVat();
 }
 
-function showDetails(){
-   const bookingDetails = document.getElementById('home-page');
-   const bookingInfo = document.getElementById('booking-info');
-   bookingDetails.style.display = "none";
-   bookingInfo.style.display = "block";
+function showDetails() {
+    const bookingDetails = document.getElementById('home-page');
+    const bookingInfo = document.getElementById('booking-info');
+    bookingDetails.style.display = "none";
+    bookingInfo.style.display = "block";
 
 }
